bc6080aaf91c81fadec2a387b5c56c4e3d82b5be
GAGAN KUMAR PARIMI <gagan110302@gmail.com>
2023-08-02 00:37:15 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3822a113fcmshf21c05e8c626ce5p1322bcjsn8a88c145bb6a",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "59192dce384e645c27110f9061c55bab"; // enter your key from openweather API
